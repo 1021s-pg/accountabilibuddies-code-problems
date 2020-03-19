48fafe80dc8914e3372d619c9cb4425b1e0ba55f
cadecameron <cade.cameron@gmail.com>
2020-03-19 10:28:05 -0700

    ADDED nextClosestTime

---
+++ nextClosestTime.js
@@ -0,0 +1,21 @@
+/* 
+
+Given a time represented in the format "HH:MM", form the next closest time by 
+reusing the current digits. There is no limit on how many times a digit can be reused.
+
+You may assume the given input string is always valid. For example, "01:34", 
+"12:09" are all valid. "1:34", "12:9" are all invalid.
+
+Example 1:
+Input: "19:34"
+Output: "19:39"
+
+Example 2:
+Input: "23:59"
+Output: "22:22"
+
+*/
+
+ function nextClosestTime(time) {
+  // TODO: solve problem
+ };
