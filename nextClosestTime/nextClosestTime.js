277a0d0dab729b5d3845b8560d259c475f0b5aeb
kmorgan8588 <kmorgan8588@gmail.com>
2020-03-22 16:21:18 -0700

    brute force solution currently with no testing

--- nextClosestTime/nextClosestTime.js
+++ nextClosestTime/nextClosestTime.js
@@ -15,7 +15,47 @@ Input: "23:59"
 Output: "22:22"
 
 */
-
+//brute force solution, 
 function nextClosestTime(time) {
-  // write your code here
+  //convert current time into hours and minutes
+  let hour = parseInt(time.substring(0, 2));
+  let minute = parseInt(time.substring(3,5));
+ 
+  const options = new Set();
+  for (let i = 0; i < time.length; i++) {
+      if(time[i] !== ":") options.add(time[i]);
+  }
+
+
+  while (true) { //tick the clock forward until we eventually reach the next valid time based on the constraints
+      if (++minute === 60) {
+        minute = 0;
+        hour++;
+        hour %= 24;
+      }
+      let current, sHour, sMin; //weird string stuff to format appropriately
+      if(hour < 10) {
+        sHour = "0" + hour;
+      } else {
+        sHour = hour;
+      }
+      if(minute < 10) {
+        sMin = "0" + minute;
+      } else {
+        sMin = minute;
+      }
+      current = sHour + ":" + sMin; // build time string
+      
+      let closest = true;
+
+      for (let i = 0; i < current.length; i++) { //check if all elements of time time string are in the options Set
+        if (current[i] === ":") continue;
+
+        if (!options.has(current[i])) { //invalid entry, break to begin while loop again
+          closest = false;
+          break;
+        }
+      }
+      if (closest) return current;
+  }
 };
